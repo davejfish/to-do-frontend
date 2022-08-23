a02e4adc6c5f8f41bf39086375aa3ab2aacabf05
Dave Fisher <dave.j.fish@gmail.com>
2022-08-23 10:01:50 -0700

    event listeners added for auth forms

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
-
+const [signUp, signIn] = document.querySelectorAll('form');
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+signUp.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('clicked');
+});
+
+signIn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('clicked as well');
+});
+
+// add function for redirect if user is logged in
